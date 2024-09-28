93be1236cc6455b3e8221bbcbe5dc9d9669277e8
Sanjay-k-m <sanjay_k.m@outlook.com>
2024-09-28 09:57:08 +0530

    env fix

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import envVar from "./config/envVar";
+import envVar from "./config/envVar.js";
+
+const REACT_APP_API_URL = envVar.REACT_APP_API_URL;
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,7 +10,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: envVar.REACT_APP_API_URL,
+        target: REACT_APP_API_URL,
       },
     },
   },
